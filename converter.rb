dd0e829a4f4a6895858928cae5eba758ba9d98b7
Alex Haase <adhaase@umich.edu>
2018-06-14 01:46:45 -0400

    initial Ruby file

---
+++ converter.rb
@@ -0,0 +1,7 @@
+# A quick single file implementation of my Overwatch conversion program - primarily for learning purposes of Ruby
+# The end version will be split up appropriately into multiple files/functions, instead of being condensed into one file. 
+
+puts "Overwatch cm/360 conversion application - Ruby version."
+puts "\n"
+
+print "Please enter your mouse DPI: " 
